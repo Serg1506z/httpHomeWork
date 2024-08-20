b3219231f1ebf51a2817842e9a2cb31ff2801d18
AppVeyor <136631034+Serg1506z@users.noreply.github.com>
2024-08-20 15:31:13 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,6 @@ module.exports = {
     'node': true
   },
   'extends': 'eslint:recommended',
-  'overrides': [
-    {
-      'files': ['**/*/*.test.js'],
-      'plugins': ['jest'],
-      'extends': ['plugin:jest/recommended'],
-      'rules': { 'jest/prefer-expect-assertions': 'off' }
-    }
-  ],
   'parserOptions': {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
